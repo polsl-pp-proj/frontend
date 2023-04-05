4d7c07da045256fbd8835e02d99774c5ffbc2fc9
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-04-05 16:03:12 +0200

    💄 Implement & style Base Modal

--- src/app/components/modals/base-modal/base-modal.component.spec.ts
+++ src/app/components/modals/base-modal/base-modal.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BaseModalComponent } from './base-modal.component';
 
 describe('BaseModalComponent', () => {
-  let component: BaseModalComponent;
-  let fixture: ComponentFixture<BaseModalComponent>;
+    let component: BaseModalComponent;
+    let fixture: ComponentFixture<BaseModalComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BaseModalComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [BaseModalComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(BaseModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(BaseModalComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
