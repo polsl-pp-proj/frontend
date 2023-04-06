a79d41fd273e684f1205ba3451032ea32867e178
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-04-06 17:39:08 +0200

    💄 Create Open Position Card

--- src/app/components/cards/open-position-card/open-position-card.component.spec.ts
+++ src/app/components/cards/open-position-card/open-position-card.component.spec.ts
@@ -1,23 +1,67 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OpenPositionCardComponent } from './open-position-card.component';
+import { By } from '@angular/platform-browser';
 
 describe('OpenPositionCardComponent', () => {
-  let component: OpenPositionCardComponent;
-  let fixture: ComponentFixture<OpenPositionCardComponent>;
+    let component: OpenPositionCardComponent;
+    let fixture: ComponentFixture<OpenPositionCardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ OpenPositionCardComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [OpenPositionCardComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(OpenPositionCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(OpenPositionCardComponent);
+        component = fixture.componentInstance;
+        component.positionName = 'Test Position';
+        component.positionDescription = 'Test description';
+        component.positionRequirements = [
+            'Requirement 1',
+            'Requirement 2',
+            'Requirement 3',
+        ];
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should display position name', () => {
+        const nameElement = fixture.debugElement.query(By.css('.name'));
+        expect(nameElement.nativeElement.textContent).toContain(
+            component.positionName
+        );
+    });
+
+    it('should display position description', () => {
+        const descriptionElement = fixture.debugElement.query(
+            By.css('.description')
+        );
+        expect(descriptionElement.nativeElement.textContent).toContain(
+            component.positionDescription
+        );
+    });
+
+    it('should display position requirements', () => {
+        const requirementsElements = fixture.debugElement.queryAll(
+            By.css('.requirements li')
+        );
+        expect(requirementsElements.length).toEqual(
+            component.positionRequirements.length
+        );
+        for (let i = 0; i < requirementsElements.length; i++) {
+            expect(requirementsElements[i].nativeElement.textContent).toContain(
+                component.positionRequirements[i]
+            );
+        }
+    });
+
+    it('should emit join event when button is clicked', () => {
+        spyOn(component.join, 'emit');
+        const buttonElement = fixture.debugElement.query(By.css('.join-btn'));
+        buttonElement.triggerEventHandler('click', null);
+        expect(component.join.emit).toHaveBeenCalled();
+    });
 });
--- src/app/components/cards/open-position-card/open-position-card.component.ts
+++ src/app/components/cards/open-position-card/open-position-card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-open-position-card',
-  templateUrl: './open-position-card.component.html',
-  styleUrls: ['./open-position-card.component.scss']
+    selector: 'app-open-position-card',
+    templateUrl: './open-position-card.component.html',
+    styleUrls: ['./open-position-card.component.scss'],
 })
 export class OpenPositionCardComponent {
+    @Input()
+    positionName!: string;
 
+    @Input()
+    positionDescription!: string;
+
+    @Input()
+    positionRequirements: string[] = [];
+
+    @Output()
+    join = new EventEmitter<void>();
 }
