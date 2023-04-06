e934462092dddfe25080442c1ad6677c66bb80ae
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-04-06 12:56:21 +0200

    ♻️ Refactor icon vault

--- src/app/modules/icon-vault/services/icon-vault.service.ts
+++ src/app/modules/icon-vault/services/icon-vault.service.ts
@@ -12,26 +12,33 @@ export class IconVaultService {
 
     public getIcon(icon: string): Observable<SafeHtml | null> {
         if (!this.icons.hasOwnProperty(icon)) {
-            this.icons[icon] = new BehaviorSubject<SafeHtml | null>(
-                undefined as unknown as SafeHtml
-            );
-
-            import(`../../../../assets/img/icons/${icon}.svg`) // ../../../../ == /src
-                .then(({ default: iconData }: { default: string }) => {
-                    this.icons[icon].next(
-                        this.sanitizer.bypassSecurityTrustHtml(iconData)
-                    );
-                })
-                .catch((e) => {
-                    this.icons[icon].error(
-                        new Error(`Requested icon '${icon}' does not exist`)
-                    );
-                });
+            this.createSubjectForIcon(icon);
+            this.importIcon(icon);
         }
 
         return this.icons[icon].asObservable().pipe(
             skipWhile((value) => value === undefined),
             take(1)
         );
+    }
+
+    private createSubjectForIcon(icon: string) {
+        this.icons[icon] = new BehaviorSubject<SafeHtml | null>(
+            undefined as unknown as SafeHtml
+        );
+    }
+
+    private importIcon(icon: string) {
+        import(`../../../../assets/img/icons/${icon}.svg`) // ../../../../ == /src
+            .then(({ default: iconData }: { default: string }) => {
+                this.icons[icon].next(
+                    this.sanitizer.bypassSecurityTrustHtml(iconData)
+                );
+            })
+            .catch((e) => {
+                this.icons[icon].error(
+                    new Error(`Requested icon '${icon}' does not exist`)
+                );
+            });
     }
 }
