75fe0767850f0280cd05e6ddca3aaab97dfb730a
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-04-13 15:40:13 +0200

    👔 Create signup FormGroup & FormControls

--- src/app/components/modals/signup-modal/signup-modal.component.ts
+++ src/app/components/modals/signup-modal/signup-modal.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-signup-modal',
     templateUrl: './signup-modal.component.html',
     styleUrls: ['./signup-modal.component.scss'],
 })
-export class SignupModalComponent {}
+export class SignupModalComponent {
+    signupForm = new FormGroup({
+        firstName: new FormControl<string>('', [Validators.required]),
+        lastName: new FormControl<string>('', [Validators.required]),
+        emailAddress: new FormControl<string>('', [
+            Validators.required,
+            Validators.email,
+        ]),
+        password: new FormControl<string>('', [Validators.required]),
+        repeatPassword: new FormControl<string>('', [Validators.required]),
+        acceptRules: new FormControl<boolean>(false, [Validators.requiredTrue]),
+    });
+
+    onClick() {
+        console.log(this.signupForm.value);
+    }
+}
