e4ba63a06824b16b6811597f5cdbcc8283526d99
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-04-13 15:43:06 +0200

    🦺 Add must match validator & error messages

--- src/app/components/modals/signup-modal/signup-modal.component.ts
+++ src/app/components/modals/signup-modal/signup-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { mustMatch } from 'src/app/validators/must-match.validator';
 
 @Component({
     selector: 'app-signup-modal',
@@ -7,17 +8,22 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
     styleUrls: ['./signup-modal.component.scss'],
 })
 export class SignupModalComponent {
-    signupForm = new FormGroup({
-        firstName: new FormControl<string>('', [Validators.required]),
-        lastName: new FormControl<string>('', [Validators.required]),
-        emailAddress: new FormControl<string>('', [
-            Validators.required,
-            Validators.email,
-        ]),
-        password: new FormControl<string>('', [Validators.required]),
-        repeatPassword: new FormControl<string>('', [Validators.required]),
-        acceptRules: new FormControl<boolean>(false, [Validators.requiredTrue]),
-    });
+    signupForm = new FormGroup(
+        {
+            firstName: new FormControl<string>('', [Validators.required]),
+            lastName: new FormControl<string>('', [Validators.required]),
+            emailAddress: new FormControl<string>('', [
+                Validators.required,
+                Validators.email,
+            ]),
+            password: new FormControl<string>('', [Validators.required]),
+            repeatPassword: new FormControl<string>('', [Validators.required]),
+            acceptRules: new FormControl<boolean>(false, [
+                Validators.requiredTrue,
+            ]),
+        },
+        { validators: mustMatch('password', 'repeatPassword', false) }
+    );
 
     onClick() {
         console.log(this.signupForm.value);
