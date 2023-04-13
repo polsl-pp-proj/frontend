2ebf30b54b7aec380185de2d7bf7a929c260f32f
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-04-13 15:36:36 +0200

    💄 Create signup modal

---
+++ src/app/components/modals/signup-modal/signup-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-signup-modal',
+    templateUrl: './signup-modal.component.html',
+    styleUrls: ['./signup-modal.component.scss'],
+})
+export class SignupModalComponent {}
