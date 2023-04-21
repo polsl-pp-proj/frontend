79f3ae283fa6cea2c14fff998a97b9311e7888ee
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-04-21 09:06:53 +0200

    💄add home page section 'recently added'

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { NotFoundPageComponent } from './views/not-found-page/not-found-page.component';
+import { HomePageComponent } from './views/home-page/home-page.component';
 
 export const routes: Routes = [
+    { path: '', component: HomePageComponent },
     { path: '404', component: NotFoundPageComponent },
     { path: '**', redirectTo: '/404' },
 ];
---
+++ src/app/views/home-page/home-page.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-home-page',
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.scss'],
+})
+export class HomePageComponent {
+    test_projects = [
+        {
+            name: 'Rakieta Elona',
+            description:
+                'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
+            image_url: 'assets/img/illustrations/pobrane.png',
+        },
+        {
+            name: 'Rakieta Elona',
+            description:
+                'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
+            image_url: 'assets/img/illustrations/pobrane.png',
+        },
+        {
+            name: 'Rakieta Elona',
+            description:
+                'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
+            image_url: 'assets/img/illustrations/pobrane.png',
+        },
+        {
+            name: 'Rakieta Elona',
+            description:
+                'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
+            image_url: 'assets/img/illustrations/pobrane.png',
+        },
+        {
+            name: 'Rakieta Elona',
+            description:
+                'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
+            image_url: 'assets/img/illustrations/pobrane.png',
+        },
+        {
+            name: 'Rakieta Elona',
+            description:
+                'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
+            image_url: 'assets/img/illustrations/pobrane.png',
+        },
+    ];
+}
