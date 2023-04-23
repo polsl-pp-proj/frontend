b5691ded0e81d60ed8d9735d558d464c74c00186
kamiluc <kamil.luc168@protonmail.com>
2023-04-23 20:17:44 +0200

    ✨ Create navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { SafeHtml } from '@angular/platform-browser';
+import { IconVaultService } from 'src/app/modules/icon-vault/services/icon-vault.service';
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent implements OnInit {
+    burgerIcon!: SafeHtml;
+    xIcon!: SafeHtml;
+
+    logged: boolean = false;
+    expandedLeftMenu: boolean = false;
+    expandedRightMenu: boolean = false;
+
+    constructor(private readonly iconVaultService: IconVaultService) {}
+
+    ngOnInit(): void {
+        this.iconVaultService
+            .getIcon('ion_menu')
+            .subscribe((icon: SafeHtml | null) => {
+                this.burgerIcon = icon!;
+            });
+        this.iconVaultService
+            .getIcon('ion_x')
+            .subscribe((icon: SafeHtml | null) => {
+                this.xIcon = icon!;
+            });
+    }
+}
