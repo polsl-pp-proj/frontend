f442d8d3660d4cd450db7f64d3a6ef4b62bcb6aa
kamiluc <kamil.luc168@protonmail.com>
2023-04-26 17:21:49 +0200

    🐛 Remove login modal from app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { ModalService } from './modules/modal/services/modal.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
-    ngAfterViewInit(): void {
-        this.modalService.updateModalState('login-modal', 'open');
-    }
+export class AppComponent {
 
-    constructor(private readonly modalService: ModalService) {}
 }
