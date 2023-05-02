23cd0b7077d30b673fe2bd3ef3fb30ff622652a7
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-05-02 14:39:39 +0200

    💄Create manage users page with dummy data

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { NotFoundPageComponent } from './views/not-found-page/not-found-page.component';
+import { ManageUsersPageComponent } from './views/manage-users-page/manage-users-page.component';
 
 export const routes: Routes = [
+    { path: 'users', component: ManageUsersPageComponent },
     { path: '404', component: NotFoundPageComponent },
     { path: '**', redirectTo: '/404' },
 ];
