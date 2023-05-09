1c0186daab5bc923562fa97ded6f29d02135f8d3
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-05-09 13:40:58 +0200

    🩹 Fix minor UI and text issues

--- src/app/views/user/project-page/project-page.component.ts
+++ src/app/views/user/project-page/project-page.component.ts
@@ -13,11 +13,11 @@ import Swiper from 'swiper';
 export class ProjectPageComponent {
     projectDto: ProjectDto = {
         id: 5,
-        name: 'Browar studencki',
-        shortDescription: 'Projekt browaru zaopatrującego igry w piwooo',
+        name: 'Projekt rakiety studenckiej',
+        shortDescription: 'Projekt rakiety studenckiej we współpracy z SpaceX.',
         description:
             'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-        projectGroupName: 'Browamators',
+        projectGroupName: 'SKN SpaceLauncher',
         assets: [
             {
                 title: 'zdj',
