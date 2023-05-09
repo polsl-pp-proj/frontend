2425d032d2430c6c48c00e82b0d31e095231ce82
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-05-09 13:56:52 +0200

    💄 Style home page correctly

--- src/app/views/user/home-page/home-page.component.ts
+++ src/app/views/user/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-home-page',
@@ -8,40 +9,52 @@ import { Component } from '@angular/core';
 export class HomePageComponent {
     test_projects = [
         {
+            id: 1,
             name: 'Rakieta Elona',
             description:
                 'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
             image_url: 'assets/img/illustrations/pobrane.png',
         },
         {
+            id: 1,
             name: 'Rakieta Elona',
             description:
                 'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
             image_url: 'assets/img/illustrations/pobrane.png',
         },
         {
+            id: 1,
             name: 'Rakieta Elona',
             description:
                 'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
             image_url: 'assets/img/illustrations/pobrane.png',
         },
         {
+            id: 1,
             name: 'Rakieta Elona',
             description:
                 'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
             image_url: 'assets/img/illustrations/pobrane.png',
         },
         {
+            id: 1,
             name: 'Rakieta Elona',
             description:
                 'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
             image_url: 'assets/img/illustrations/pobrane.png',
         },
         {
+            id: 1,
             name: 'Rakieta Elona',
             description:
                 'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
             image_url: 'assets/img/illustrations/pobrane.png',
         },
     ];
+
+    constructor(private readonly router: Router) {}
+
+    visitProject(projectId: number) {
+        this.router.navigate(['project', projectId]);
+    }
 }
