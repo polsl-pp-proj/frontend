341d192fdd78eb7d13ba0929b4a845077050ca30
kamiluc <kamil.luc168@protonmail.com>
2023-05-22 14:34:01 +0200

    :adhesive_bandage: Resolve comments

--- src/app/components/modals/create-organization-modal/create-organization-modal.component.ts
+++ src/app/components/modals/create-organization-modal/create-organization-modal.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { CreateOrganizationDto } from 'src/app/dtos/create-organization-dto';
 
 @Component({
     selector: 'app-create-organization-modal',
@@ -9,21 +8,20 @@ import { CreateOrganizationDto } from 'src/app/dtos/create-organization-dto';
 })
 export class CreateOrganizationModalComponent implements OnInit {
     createOrganizationForm: FormGroup;
+    organizationName: String = '';
     sent: boolean = false;
-
-    createOrganizationDto = new CreateOrganizationDto('');
 
     constructor() {
         this.createOrganizationForm = new FormGroup({
-            organizationName: new FormControl(null, [Validators.required]),
+            organizationName: new FormControl('', [Validators.required]),
         });
     }
 
     ngOnInit(): void {}
 
     onOrganizationCreate() {
-        this.sent = true;
-        this.createOrganizationDto.organizationName =
+        this.organizationName =
             this.createOrganizationForm.get('organizationName')?.value;
+        this.sent = true;
     }
 }
--- src/app/dtos/create-organization-dto.ts
+++
@@ -1,7 +0,0 @@
-export class CreateOrganizationDto {
-    organizationName: string;
-
-    constructor(organizationName: string) {
-        this.organizationName = organizationName;
-    }
-}
