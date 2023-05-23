1ea20998dd419cde042109f7a0c9140c0a3efd94
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-05-23 13:49:29 +0200

    ✨ Hydrate set new password modal

--- src/app/components/modals/set-new-password-modal/set-new-password-modal.component.ts
+++ src/app/components/modals/set-new-password-modal/set-new-password-modal.component.ts
@@ -1,12 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
-import {
-    AbstractControl,
-    FormControl,
-    FormGroup,
-    ValidationErrors,
-    ValidatorFn,
-    Validators,
-} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/modules/auth/services/auth.service';
+import { ModalService } from 'src/app/modules/modal/services/modal.service';
 import { mustMatch } from 'src/app/validators/must-match.validator';
 
 @Component({
@@ -15,23 +12,72 @@ import { mustMatch } from 'src/app/validators/must-match.validator';
     styleUrls: ['./set-new-password-modal.component.scss'],
 })
 export class SetNewPasswordModalComponent {
+    static ModalName = 'set-new-password-modal';
+    get modalName() {
+        return SetNewPasswordModalComponent.ModalName;
+    }
+
     setNewPasswordForm = new FormGroup(
         {
-            newPassword: new FormControl<string>('', [
-                Validators.required,
-                Validators.maxLength(20),
-                Validators.minLength(5),
-            ]),
-            repeatNewPassword: new FormControl<string>('', [
-                Validators.required,
-                Validators.maxLength(20),
-                Validators.minLength(5),
-            ]),
+            newPassword: new FormControl<string>('', {
+                nonNullable: true,
+                validators: [Validators.required, Validators.minLength(5)],
+            }),
+            repeatNewPassword: new FormControl<string>('', {
+                nonNullable: true,
+                validators: [Validators.required, Validators.minLength(5)],
+            }),
         },
         { validators: mustMatch('newPassword', 'repeatNewPassword', false) }
     );
 
-    sendSetNewPasswordRequest() {
-        //tbd
+    inTransit = false;
+
+    constructor(
+        private readonly authService: AuthService,
+        private readonly toastrService: ToastrService,
+        private readonly modalService: ModalService
+    ) {}
+
+    setNewPassword() {
+        if (this.setNewPasswordForm.valid) {
+            this.inTransit = true;
+            this.authService
+                .confirmPasswordReset(
+                    this.setNewPasswordForm.controls.newPassword.value
+                )
+                .subscribe({
+                    next: () => {
+                        this.toastrService.success(
+                            'Twoje hasło zostało zresetowane.',
+                            'Hasło zresetowane'
+                        );
+                        this.modalService.updateModalState(
+                            this.modalName,
+                            'close'
+                        );
+                        this.setNewPasswordForm.reset();
+                        this.inTransit = false;
+                    },
+                    error: (err: HttpErrorResponse) => {
+                        this.inTransit = false;
+                        switch (err.status) {
+                            case 404: {
+                                this.toastrService.error(
+                                    'Link resetowania jest niepoprawny lub wygasł!',
+                                    'Resetowanie hasła nie powiodło się'
+                                );
+                                break;
+                            }
+                            default: {
+                                this.toastrService.error(
+                                    'Podczas resetowania hasła wystąpił błąd. Spróbuj ponownie później.',
+                                    'Resetowanie hasła nie powiodło się'
+                                );
+                            }
+                        }
+                    },
+                });
+        }
     }
 }
