2a950c58e9860e13284c1ae317268b6cb1c1f60a
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-06-14 13:15:57 +0200

    🐛fix according to comments

--- src/app/views/admin/category-page/category-page.component.ts
+++ src/app/views/admin/category-page/category-page.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-category-page',
-  templateUrl: './category-page.component.html',
-  styleUrls: ['./category-page.component.scss']
+    selector: 'app-category-page',
+    templateUrl: './category-page.component.html',
+    styleUrls: ['./category-page.component.scss'],
 })
 export class CategoryPageComponent {
-
+    categoryList: { name: string; amount: number }[] = [
+        { name: 'Elektronika', amount: 10 },
+        { name: 'Paduch', amount: 666 },
+        { name: 'Oleg', amount: 7 },
+    ];
 }
