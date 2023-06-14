802e234cc93f512c69fe3acdf288f12d67157e54
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-06-14 18:58:14 +0200

    ✨ Create NotificationEventType enum

---
+++ src/app/modules/notification/enums/notification-event-type.enum.ts
@@ -0,0 +1,7 @@
+export enum NotificationEventType {
+    CloseConnection = 'close_connection',
+    NotificationCreated = 'notification:created',
+    NotificationSeen = 'notification:seen',
+    NotificationNotSeen = 'notification:not-seen',
+    NotificationRemoved = 'notification:removed',
+}
