9c48d1610b8fe41085b43636578a61877ac37e18
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-06-14 13:37:26 +0200

    🐛 Fix according to comments

--- src/app/views/admin/manage-users-page/manage-users-page.component.ts
+++ src/app/views/admin/manage-users-page/manage-users-page.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { UserOrganizationDto } from 'src/app/modules/auth/dtos/user-organization.dto';
 
 @Component({
-  selector: 'app-manage-users-page',
-  templateUrl: './manage-users-page.component.html',
-  styleUrls: ['./manage-users-page.component.scss']
+    selector: 'app-manage-users-page',
+    templateUrl: './manage-users-page.component.html',
+    styleUrls: ['./manage-users-page.component.scss'],
 })
 export class ManageUsersPageComponent {
-
+    usersList: {
+        name: string;
+        lastname: string;
+        role: string;
+        email: string;
+        student: string;
+    }[] = [
+        {
+            name: 'Pad',
+            lastname: 'Paduch',
+            email: 'test@test.com',
+            role: 'Moderator',
+            student: 'Nie',
+        },
+        {
+            name: 'Pad',
+            lastname: 'Oleg',
+            email: 'test@test.com',
+            role: 'Student',
+            student: 'Tak',
+        },
+        {
+            name: 'Pad',
+            lastname: 'Zielinski',
+            email: 'test@test.com',
+            role: 'Administrator',
+            student: 'Nie',
+        },
+    ];
 }
