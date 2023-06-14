fa006e9a66fb939d48e79da5b4d9903fdde065bd
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-06-14 14:29:04 +0200

    ✨ Load universities, waiting for backend

--- src/app/components/modals/verify-student-modal/verify-student-modal.component.ts
+++ src/app/components/modals/verify-student-modal/verify-student-modal.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Subscription, map } from 'rxjs';
+import { PolonService } from 'src/app/modules/polon/services/polon.service';
 @Component({
     selector: 'app-verify-student-modal',
     templateUrl: './verify-student-modal.component.html',
     styleUrls: ['./verify-student-modal.component.scss'],
 })
-export class VerifyStudentModalComponent {
+export class VerifyStudentModalComponent implements OnInit {
     static ModalName = 'verify-student-modal';
     get modalName() {
         return VerifyStudentModalComponent.ModalName;
     }
+    private universitiesSubscription!: Subscription;
 
     form = new FormGroup({
         university: new FormControl<string>('', {
@@ -22,9 +25,23 @@ export class VerifyStudentModalComponent {
         }),
     });
 
-    universities: { text: string; value: string }[] = [
-        { text: 'szkola melanzu', value: '1' },
-        { text: 'szkola melanzu', value: '1' },
-        { text: 'szkola melanzu', value: '1' },
-    ];
+    constructor(private polonService: PolonService) {}
+
+    universities: { text: string; value: string }[] = [];
+
+    private loadUniversities(): void {
+        this.universitiesSubscription = this.polonService
+            .getAcademicInstitutions('OPERATING')
+            .subscribe((institutions) => {
+                console.log(institutions);
+                this.universities = institutions.map((institution) => ({
+                    text: institution.name,
+                    value: institution.uid,
+                }));
+            });
+    }
+
+    ngOnInit(): void {
+        this.loadUniversities();
+    }
 }
