995a9382834a27fc39a3f26a6994532031160f4e
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-06-15 18:45:36 +0200

    ✨ Hydrate projects moderation page

--- src/app/modules/submission/modules/submission-api/dtos/submission.dto.ts
+++ src/app/modules/submission/modules/submission-api/dtos/submission.dto.ts
@@ -2,8 +2,7 @@ import { SubmissionStatus } from '../enums/submission-status.enum';
 
 export class SubmissionDto {
     id!: number;
-    projectDraftName!: string;
-    projectDraftId!: number;
     status!: SubmissionStatus;
     createdAt!: number;
+    simpleProjectDraft: SimpleProjectDto;
 }
--- src/app/views/admin/projects-moderation-page/projects-moderation-page.component.ts
+++ src/app/views/admin/projects-moderation-page/projects-moderation-page.component.ts
@@ -1,53 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SubmissionDto } from 'src/app/modules/submission/modules/submission-api/dtos/submission.dto';
+import { SubmissionService } from 'src/app/modules/submission/services/submission.service';
 
 @Component({
     selector: 'app-projects-moderation-page',
     templateUrl: './projects-moderation-page.component.html',
     styleUrls: ['./projects-moderation-page.component.scss'],
 })
-export class ProjectsModerationPageComponent {
-    test_projects = [
-        {
-            id: 1,
-            name: 'Rakieta Elona',
-            description:
-                'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
-            image_url: 'assets/img/illustrations/pobrane.png',
-        },
-        {
-            id: 1,
-            name: 'Rakieta Elona',
-            description:
-                'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
-            image_url: 'assets/img/illustrations/pobrane.png',
-        },
-        {
-            id: 1,
-            name: 'Rakieta Elona',
-            description:
-                'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
-            image_url: 'assets/img/illustrations/pobrane.png',
-        },
-        {
-            id: 1,
-            name: 'Rakieta Elona',
-            description:
-                'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
-            image_url: 'assets/img/illustrations/pobrane.png',
-        },
-        {
-            id: 1,
-            name: 'Rakieta Elona',
-            description:
-                'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
-            image_url: 'assets/img/illustrations/pobrane.png',
-        },
-        {
-            id: 1,
-            name: 'Rakieta Elona',
-            description:
-                'Super szybka rakieta lecaca z predkoscia swiatla, mega szybka jest!!!!',
-            image_url: 'assets/img/illustrations/pobrane.png',
-        },
-    ];
+export class ProjectsModerationPageComponent implements OnInit {
+    projectSubmissions: SubmissionDto[] = [];
+
+    constructor(
+        private readonly submissionService: SubmissionService,
+        private readonly router: Router
+    ) {}
+
+    ngOnInit() {
+        this.submissionService.getSubmissions().subscribe((submissions) => {
+            this.projectSubmissions = submissions;
+        });
+    }
+
+    goToProjectModeration(submissionId: number) {
+        this.router.navigate(['/admin', 'moderate', 'project', submissionId]);
+    }
 }
