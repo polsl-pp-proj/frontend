800cc7a6d15a1ef46d883a0d6df8bcc30c41e993
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2023-06-19 14:00:34 +0200

    ✨ Hydrate own organizations and favorite projects

--- src/app/modules/favorite/services/favorite.service.ts
+++ src/app/modules/favorite/services/favorite.service.ts
@@ -2,30 +2,35 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, catchError, map, tap } from 'rxjs';
 import { FavoriteApiService } from '../modules/favorite-api/services/favorite-api.service';
+import { SimpleProjectDto } from 'src/app/dtos/simple-project.dto';
 
 @Injectable({
     providedIn: 'root',
 })
 export class FavoriteService {
-    private favorites = new BehaviorSubject<number[]>([]);
+    private simpleFavorites = new BehaviorSubject<number[]>([]);
 
     constructor(private readonly favoriteApiService: FavoriteApiService) {}
 
     fetchFavorites() {
         return this.favoriteApiService.getSimpleFavorites().pipe(
-            tap((favoriteIds) => this.favorites.next(favoriteIds)),
+            tap((favoriteIds) => this.simpleFavorites.next(favoriteIds)),
             map(() => {
                 return;
             })
         );
     }
 
+    getFullFavorites() {
+        return this.favoriteApiService.getFullFavorites();
+    }
+
     clearFavorites() {
-        this.favorites.next([]);
+        this.simpleFavorites.next([]);
     }
 
     isFavorite(projectId: number) {
-        return this.favorites.pipe(
+        return this.simpleFavorites.pipe(
             map((favoriteIds) => {
                 return favoriteIds.some(
                     (favoriteId) => favoriteId === projectId
@@ -35,10 +40,10 @@ export class FavoriteService {
     }
 
     addToFavorites(projectId: number) {
-        const favorites = new Set<number>(this.favorites.value);
+        const favorites = new Set<number>(this.simpleFavorites.value);
         if (!favorites.has(projectId)) {
             favorites.add(projectId);
-            this.favorites.next(Array.from(favorites));
+            this.simpleFavorites.next(Array.from(favorites));
         }
         return this.favoriteApiService.addToFavorites(projectId).pipe(
             catchError((err) => {
@@ -53,10 +58,10 @@ export class FavoriteService {
     }
 
     removeFromFavorites(projectId: number) {
-        const favorites = new Set<number>(this.favorites.value);
+        const favorites = new Set<number>(this.simpleFavorites.value);
         if (favorites.has(projectId)) {
             favorites.delete(projectId);
-            this.favorites.next(Array.from(favorites));
+            this.simpleFavorites.next(Array.from(favorites));
         }
         return this.favoriteApiService.removeFromFavorites(projectId).pipe(
             catchError((err) => {
--- src/app/views/user/my-account-page/my-account-page.component.ts
+++ src/app/views/user/my-account-page/my-account-page.component.ts
@@ -5,13 +5,17 @@ import { ChangePasswordModalComponent } from 'src/app/components/modals/change-p
 import { VerifyStudentModalComponent } from 'src/app/components/modals/verify-student-modal/verify-student-modal.component';
 import { PastPaymentDto } from 'src/app/dtos/past-payment.dto';
 import { ProjectCardDto } from 'src/app/dtos/project-card.dto';
+import { SimpleProjectDto } from 'src/app/dtos/simple-project.dto';
 import { AuthTokenPayloadDto } from 'src/app/modules/auth/dtos/auth-token-payload.dto';
 import { UserOrganizationDto } from 'src/app/modules/auth/dtos/user-organization.dto';
 import { UserRole } from 'src/app/modules/auth/enums/user-role.enum';
 import { AuthService } from 'src/app/modules/auth/services/auth.service';
 import { StudentshipService } from 'src/app/modules/auth/services/studentship.service';
+import { FavoriteService } from 'src/app/modules/favorite/services/favorite.service';
 import { ModalService } from 'src/app/modules/modal/services/modal.service';
 import { OrganizationMemberRole } from 'src/app/modules/organization/enums/organization-member-role.enum';
+import { OrganizationDto } from 'src/app/modules/organization/modules/organization-api/dtos/organization.dto';
+import { OrganizationService } from 'src/app/modules/organization/services/organization.service';
 
 @Component({
     selector: 'app-my-account-page',
@@ -19,25 +23,8 @@ import { OrganizationMemberRole } from 'src/app/modules/organization/enums/organ
     styleUrls: ['./my-account-page.component.scss'],
 })
 export class MyAccountPageComponent implements OnInit, OnDestroy {
-    authPayloadSubscription!: Subscription;
+    subsink: Subscription[] = [];
     payload!: AuthTokenPayloadDto;
-
-    constructor(
-        private readonly authService: AuthService,
-        private readonly router: Router,
-        private readonly modalService: ModalService
-    ) {}
-
-    projects: ProjectCardDto[] = [
-        {
-            projectId: 2,
-            imageUrl: 'https://placehold.co/1200x630',
-            imageAlt: 'alt',
-            projectName: 'rakieta',
-            projectDescription: 'fajna rakieta',
-            projectOrg: 'Zmitac studio original',
-        },
-    ];
 
     payments: PastPaymentDto[] = [];
 
@@ -46,44 +33,65 @@ export class MyAccountPageComponent implements OnInit, OnDestroy {
         [UserRole.Moderator]: 'Moderator',
         [UserRole.Administrator]: 'Administrator',
     };
+    organizationRoles = {
+        [OrganizationMemberRole.Member]: 'Członek',
+        [OrganizationMemberRole.Owner]: 'Właściciel',
+    };
 
-    organizations: UserOrganizationDto[] = [
-        {
-            organizationId: 1,
-            role: OrganizationMemberRole.Owner,
-        },
-        {
-            organizationId: 2,
-            role: OrganizationMemberRole.Member,
-        },
-        {
-            organizationId: 3,
-            role: OrganizationMemberRole.Member,
-        },
-    ];
+    organizations: OrganizationDto[] = [];
+
+    get organizationsWithRoles(): (OrganizationDto & {
+        role: OrganizationMemberRole;
+    })[] {
+        return this.organizations.map((organization) => ({
+            ...organization,
+            role:
+                this.payload.organizations.find(
+                    (userOrg) => userOrg.organizationId === organization.id
+                )?.role ?? OrganizationMemberRole.Member,
+        }));
+    }
+
+    favoriteProjects: SimpleProjectDto[] = [];
+
+    constructor(
+        private readonly authService: AuthService,
+        private readonly router: Router,
+        private readonly modalService: ModalService,
+        private readonly organizationService: OrganizationService,
+        private readonly favoriteService: FavoriteService
+    ) {}
 
     ngOnInit(): void {
-        this.authPayloadSubscription = this.authService.authTokenPayload
-            .pipe(skipWhile((payload) => payload === undefined))
-            .subscribe((payload) => {
-                if (payload) {
-                    this.payload = payload;
-                    return;
-                }
-                this.router.navigate(['/']);
+        this.subsink.push(
+            this.authService.authTokenPayload
+                .pipe(skipWhile((payload) => payload === undefined))
+                .subscribe((payload) => {
+                    if (payload) {
+                        this.payload = payload;
+                        return;
+                    }
+                    this.router.navigate(['/']);
+                })
+        );
+
+        this.organizationService
+            .getOwnOrganizations()
+            .subscribe((organizations) => {
+                this.organizations = organizations;
             });
 
-        for (let i = 0; i < 14; i++) {
-            this.projects.push(this.projects[0]);
-        }
+        this.favoriteService
+            .getFullFavorites()
+            .subscribe((favorites) => (this.favoriteProjects = favorites));
     }
 
     ngOnDestroy(): void {
-        this.authPayloadSubscription.unsubscribe();
+        this.subsink.forEach((sub) => sub.unsubscribe());
     }
 
     visitProject(projectId: number) {
-        this.router.navigate(['project', projectId]);
+        this.router.navigate(['/project', projectId]);
     }
 
     verifyStudentship() {
